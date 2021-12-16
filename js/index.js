0b9b4e2406823b6b1b8dace2f1019e366c4ae844
DidiAbel <didiabel123@gmail.com>
2021-12-16 15:53:54 -0300

    commit2

--- js/index.js
+++ js/index.js
@@ -11,16 +11,19 @@ let humedad = document.querySelector("#humedad")
 let viento = document.querySelector("#viento")
 
 const peticionClima = async() => {
+
+    const kelviToCelsius = (kelvi) => {
+        return parseInt(kelvi-273.15).toFixed()
+    }
     const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityname.value} &appid=7ca589cf4950e4cd0136f0a24e55c404`)
     const ciudad = await response.json()
 
     textoCiudad.textContent = ciudad.name
-    temperatura.textContent= parseInt(ciudad.main.temp-273.15).toFixed()+ " C"; 
+    temperatura.textContent= kelviToCelsius(ciudad.main.temp) + " C"; 
     icono.src = `http://openweathermap.org/img/w/${ciudad.weather[0].icon}.png`
     pronostico.textContent = ciudad.weather[0].description
     humedad.textContent = ciudad.main.humidity
     viento.textContent = ciudad.wind.speed
-    
 }
 
 btn.addEventListener("click", peticionClima)
